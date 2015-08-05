889d222dc2f0799b13d78bec2e3767051be16514
Shohei Aoki <shoaok@gmail.com>
2015-08-05 14:13:07 -0400

    update

--- lib/shiny/generator/scraper.rb
+++ lib/shiny/generator/scraper.rb
@@ -2,7 +2,12 @@ module Shiny
   module Generator
     class Scraper
       def initialize
+        #for debug
         puts "hello world"
+      end
+
+      def 
+        
       end
     end
   end
--- sample/sample.rb
+++ sample/sample.rb
@@ -1,8 +1,19 @@
 require 'rubygems'
+require 'opal'
+require 'opal-jquery'
+
 $:.unshift File.expand_path("./lib"), File.dirname(__FILE__)
 
 require 'shiny/generator'
 include Shiny::Generator
 
 s = Scraper.new
+
+
+
+
+HTTP.get("https://itunes.apple.com/search?term=jack+johnson") do |response|
+  puts response.body
+  # => "{\"name\": \"Adam Beynon\"}"
+end
 
